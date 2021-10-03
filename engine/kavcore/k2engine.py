70d0e5eef5c50206b10d64e334215e3ab17eada9
KICKER <gksdbfla12@naver.com>
2021-10-04 00:20:41 +0900

    [mod]k2.py, k2engine.py k2kmdfile.py k2timelib.py

--- engine/kavcore/k2engine.py
+++ engine/kavcore/k2engine.py
@@ -1,6 +1,5 @@
 # -*- coding:utf-8 -*-
 
-import sys
 import os
 import datetime
 import imp
@@ -8,6 +7,7 @@ import tempfile
 import types
 import mmap
 import glob
+
 import k2timelib
 import k2kmdfile
 import k2rsa
