5449a1d8371fe0098d912d9047ab4fe5a44153d6
Woojin-Crive <dnldnwls1123@gmail.com>
2021-10-04 18:16:17 +0900

    import fixed

--- engine/kavcore/k2rc4.py
+++ engine/kavcore/k2rc4.py
@@ -111,11 +111,11 @@ class RC4:
 
 # TEST 코드
 if __name__=='__main__':
-    rc4 = k2rc4.RC4()
+    rc4 = RC4()
     rc4.set_key('PASSWORD1234') # 암호 설정
     t1 = rc4.crypt('hello')
 
-    rc4 = k2rc4.RC4()
+    rc4 = RC4()
     rc4.set_key('PASSWORD1234')
     t2 = rc4.crypt(t1) # 복호화
     print (t2)
